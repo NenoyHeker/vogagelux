70bf8e2431d2424d74a10b6adb60f7bd323ec586
Chris484AR <106822660+Chris484AR@users.noreply.github.com>
2023-10-23 22:14:20 -0600

    Add files via upload

---
+++ db/db.js
@@ -0,0 +1,16 @@
+//Base de datos
+//Base de datos
+const db = [
+    {id: 1, nombre: 'Juan'},
+    {id: 2, nombre: 'Pedro'},
+    {id: 3, nombre: 'María'},
+    {id: 4, nombre: 'Luis'},
+    {id: 5, nombre: 'Martha'},
+    {id: 6, nombre: 'José'},
+    {id: 7, nombre: 'Daniel'},
+    {id: 8, nombre: 'Alejandro'},
+    {id: 9, nombre: 'Alicia'},
+    {id: 10, nombre: 'Miguel'},
+]
+
+module.exports = db
